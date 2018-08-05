ad1655729a7a56f5fae2ac800a60a16692b09d92
danielyuan911 <hbycyf123@163.com>
2018-08-05 16:16:39 +0800

    add chapter 10

---
+++ Chapter10/src/main/java/com/course/server/MyGetMethod.java
@@ -0,0 +1,81 @@
+package com.course.server;
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@RestController
+public class MyGetMethod {
+
+    /**
+     * 返回信息中添加cookie信息
+     */
+    @RequestMapping(value = "/getCookies",method = RequestMethod.GET)
+    public String getCookies(HttpServletResponse response){
+
+        //HttpServerletRequest
+        //HttpServerletResponse
+        Cookie cookie = new Cookie("login","true");
+        response.addCookie(cookie);
+        return "恭喜你获取cookies成功";
+    }
+
+    /**
+     * 要求客户端携带cookies访问
+     * 通过Jmeter访问时带上cookie信息（cookie manager)
+     */
+    @RequestMapping(value = "/get/with/cookies",method = RequestMethod.GET)
+    public String getWithCookies(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if(Objects.isNull((cookies))){
+            return "你必须携带cookies信息来";
+        }
+
+        for(Cookie cookie:cookies){
+            if(cookie.getName().equals("login") &&cookie.getValue().equals("true")){
+                return "恭喜你访问成功,这是一个必须携带cookies信息才能访问的get请求";
+            }
+        }
+        return "你必须携带cookies信息来";
+    }
+
+    /**
+     * 开发一个需要携带参数才能访问的get请求
+     * 第一种实现方式 url: key=value&key=value
+     * 我们来模拟获取商品列表
+     */
+    @RequestMapping(value = "/get/with/param",method = RequestMethod.GET)
+    public Map<String,Integer> getList(@RequestParam Integer start, @RequestParam Integer end){
+
+        Map<String,Integer> myList = new HashMap<>();
+
+        myList.put("鞋",400);
+        myList.put("干脆面",1);
+        myList.put("衬衫",300);
+
+        return myList;
+    }
+
+    /**
+     * 第二种需要携带参数访问的get请求
+     * url:ip/port/get/with/param/10/20
+     */
+
+    @RequestMapping(value = "/get/with/param/{start}/{end}")
+    public Map<String,Integer> myGetList(@PathVariable Integer start,
+                         @PathVariable Integer end){
+
+        Map<String,Integer> myList = new HashMap<>();
+
+        myList.put("鞋",400);
+        myList.put("干脆面",1);
+        myList.put("衬衫",300);
+
+        return myList;
+    }
+}
